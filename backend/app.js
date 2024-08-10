31bdfa4b37b72242179929b21b2e4a68cf2e742a
arunchauhan02 <100798589+arunchauhan02@users.noreply.github.com>
2024-08-10 18:53:09 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,13 +6,16 @@ const cors = require('cors');
 const bodyParser = require("body-parser")
 const fileUpload = require("express-fileupload")
 const path = require("path");
-const cors = require("cors");
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
     require('dotenv').config({path:"backend/config/config.env"});
 }
 
-app.use(cors())
+const corsOptions = {
+    origin: true,
+    credentials: true,
+  }
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}))
@@ -31,12 +34,6 @@ app.use("/api/v1",user)
 app.use("/api/v1",order)
 app.use("/api/v1",collection);
 app.use("/api/v1",payment)
-
-const corsOptions = {
-    origin: true,
-    credentials: true,
-  }
-app.use(cors(corsOptions));
 
 app.use(errMiddleWare)
 
