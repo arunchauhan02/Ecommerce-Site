64977d5dec5eb6df593fe460287289440e96424f
arunchauhan02 <100798589+arunchauhan02@users.noreply.github.com>
2024-08-12 16:34:18 +0530

    Update paymentRoute.js

--- backend/routes/paymentRoute.js
+++ backend/routes/paymentRoute.js
@@ -4,6 +4,6 @@ const {isauthenticated} = require("../middleware/auth");
 const { processPayment, sendApiKey } = require("../controllers/paymentController");
 
 router.route("/payment/process").post(isauthenticated,processPayment)
-router.route("/stripeapikey").post(isauthenticated,sendApiKey);
+router.route("/stripeapikey").post(sendApiKey);
 
 module.exports = router;
